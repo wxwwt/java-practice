2a0f7944354e0a2475fd48736ffb6eade8de7e07
chuang.shen <chuang.shen@manaowan.com>
2019-05-23 23:18:07 +0800

    最长回文串问题提交：时间复杂度只超过了18%的人，空间复杂度小于98%的人 后面看其他人是什么思路，再改改这臃肿的代码

--- src/main/java/com/scott/java/task/algorithm/LongestPalindromicSubstring.java
+++ src/main/java/com/scott/java/task/algorithm/LongestPalindromicSubstring.java
@@ -18,44 +18,44 @@ public class LongestPalindromicSubstring {
         System.out.println(LongestPalindromicSubstring.longestPalindrome("babad"));
         System.out.println(LongestPalindromicSubstring.longestPalindrome("cbbd"));
         System.out.println(LongestPalindromicSubstring.longestPalindrome(""));
+
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("aaaa"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("aaaaa"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("abcdfg"));
+        /**
+         * 输出结果：
+         * ababa
+         * bab
+         * bb
+         *
+         * aaaa
+         * aaaaa
+         * a
+         */
+
     }
 
     public static String longestPalindrome(String s) {
-        if (s.length() > 1000 || s == null) {
+        if (null == s || s.length() > 1000) {
             return null;
         }
         if (s.equals("")) {
             return "";
         }
-
         char[] charArr = s.toCharArray();
         // 使用双向队列来保存最大的子串
-        Deque<Character> deque = new ArrayDeque();
-        String longestStr = String.valueOf(charArr[0]);
+        Deque<Character> deque = new ArrayDeque<>();
+        Character[] maxArr = new Character[]{charArr[0]};
+        String longestStr;
         int maxInt = 0;
-
-
         for (int i = 0; i < charArr.length; i++) {
-            deque.clear();
             int head = 0;
             int tail = 0;
-            int flag = 0;
             // 偶数回文  用来判断回文数的两个数字的下标不能超过最大下标
             if (i + 1 <= charArr.length - 1 && charArr[i] == charArr[i + 1]) {
+                deque.clear();
                 head = i;
                 tail = i + 1;
-                flag = 1;
-            }
-            // 奇数回文
-            if (i + 2 <= charArr.length - 1 && charArr[i] == charArr[i + 2]) {
-                head = i;
-                tail = i + 2;
-                flag = 1;
-                // 奇数的把中间轴对称的数先放入双向队列
-                deque.addFirst(charArr[i + 1]);
-            }
-            // 有相等的数才循环 否则 直接略过
-            if (flag == 1) {
                 while (head >= 0 && tail <= charArr.length - 1) {
                     if (charArr[head] == charArr[tail]) {
                         deque.addFirst(charArr[head]);
@@ -66,17 +66,40 @@ public class LongestPalindromicSubstring {
                         break;
                     }
                 }
-            }
-            if (deque.size() > maxInt) {
-                maxInt = deque.size();
-                Character[] aa = deque.toArray(new Character[deque.size()]);
-                char[] bb = new char[1000];
-                for (int j = 0; j < aa.length; j++) {
-                    bb[j] = aa[j];
+                if (deque.size() > maxInt) {
+                    maxInt = deque.size();
+                    maxArr = deque.toArray(new Character[deque.size()]);
                 }
-                longestStr = String.valueOf(bb).trim();
+            }
+            // 奇数回文
+            if (i + 2 <= charArr.length - 1 && charArr[i] == charArr[i + 2]) {
+                deque.clear();
+                head = i;
+                tail = i + 2;
+                // 奇数的把中间轴对称的数先放入双向队列
+                deque.addFirst(charArr[i + 1]);
+                while (head >= 0 && tail <= charArr.length - 1) {
+                    if (charArr[head] == charArr[tail]) {
+                        deque.addFirst(charArr[head]);
+                        deque.addLast(charArr[tail]);
+                        --head;
+                        ++tail;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (deque.size() > maxInt) {
+                    maxInt = deque.size();
+                    maxArr = deque.toArray(new Character[deque.size()]);
+                }
             }
         }
+        char[] bb = new char[1000];
+        for (int j = 0; j < maxArr.length; j++) {
+            bb[j] = maxArr[j];
+        }
+        longestStr = String.valueOf(bb).trim();
         return longestStr;
     }
 }
